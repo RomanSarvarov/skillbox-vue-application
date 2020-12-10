29d14948aa5351649e3c71e7f1b8f6a88e188311
Roman Sarvarov <roman.sarvarov@gmail.com>
2020-12-11 00:54:11 +0300

    part 3.3 done

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    name: 'Радионяня Motorola MBP16',
+    image: '/img/radio.jpg',
+    price: 3690,
+  },
+  {
+    name: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    image: '/img/toothbrush.jpg',
+    price: 5660,
+  },
+  {
+    name: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    image: '/img/phone.jpg',
+    price: 21790,
+  },
+  {
+    name: 'Электроскейт Razor Cruiser',
+    image: '/img/board.jpg',
+    price: 24690,
+  },
+  {
+    name: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    image: '/img/phone-2.jpg',
+    price: 14960,
+  },
+  {
+    name: 'Смартфон Xiaomi Redmi 6/128GB',
+    image: '/img/phone-3.jpg',
+    price: 8960,
+  },
+  {
+    name: 'Электрический дрифт-карт Razor Crazy Cart',
+    image: '/img/bicycle.jpg',
+    price: 39900,
+  },
+  {
+    name: 'Гироскутер Razor Hovertrax 2.0',
+    image: '/img/wheels.jpg',
+    price: 34900,
+  },
+  {
+    name: 'Детский трюковой самокат Razor Grom',
+    image: '/img/scooter.jpg',
+    price: 4990,
+  },
+  {
+    name: 'Роллерсёрф Razor RipStik Air Pro',
+    image: '/img/ripstik.jpg',
+    price: 6690,
+  },
+  {
+    name: 'Наушники AirPods с беспроводным зарядным футляром',
+    image: '/img/airpods.jpg',
+    price: 16560,
+  },
+  {
+    name: 'Наушники Sony',
+    image: '/img/headphones.jpg',
+    price: 30690,
+  },
+];
