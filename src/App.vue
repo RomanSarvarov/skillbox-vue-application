76bd3efe2463d03552cde149c586a589ad995393
Roman Sarvarov <roman.sarvarov@gmail.com>
2020-12-14 23:32:03 +0300

    move product filters to component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <section class="catalog">
-    <ProductList :products="products" />
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
 
-    <BasePagination v-model:page="page" :count="countProducts" :per-page="perPage"/>
-  </section>
+    <div class="content__catalog">
+      <ProductFilter />
+
+      <section class="catalog">
+        <ProductList :products="products" />
+
+        <BasePagination v-model:page="page" :count="countProducts" :per-page="perPage"/>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
 import BasePagination from '@/components/BasePagination';
+import ProductFilter from '@/components/products/ProductFilter';
 import ProductList from './components/products/ProductList';
 import products from './data/products';
 
 export default {
   name: 'App',
-  components: { BasePagination, ProductList },
+  components: { ProductFilter, BasePagination, ProductList },
   data() {
     return {
       page: 1,
